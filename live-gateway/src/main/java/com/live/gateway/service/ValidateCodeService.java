024a5b4f1e82dfd5870365809560c6b14d4594c2
wy <lim666067@gmail.com>
2024-10-30 18:21:08 +0800

    init

--- live-gateway/src/main/java/com/live/gateway/service/ValidateCodeService.java
+++ live-gateway/src/main/java/com/live/gateway/service/ValidateCodeService.java
@@ -1,24 +1,24 @@
-package com.live.gateway.service;
-
-import com.live.common.core.exception.CaptchaException;
-import com.live.common.core.web.domain.AjaxResult;
-
-import java.io.IOException;
-
-/**
- * 验证码处理
- *
- * @author live
- */
-public interface ValidateCodeService
-{
-    /**
-     * 生成验证码
-     */
-    public AjaxResult createCaptcha() throws IOException, CaptchaException;
-
-    /**
-     * 校验验证码
-     */
-    public void checkCaptcha(String key, String value) throws CaptchaException;
-}
+//package com.live.gateway.service;
+//
+//import com.live.common.core.exception.CaptchaException;
+//import com.live.common.core.web.domain.AjaxResult;
+//
+//import java.io.IOException;
+//
+///**
+// * 验证码处理
+// *
+// * @author live
+// */
+//public interface ValidateCodeService
+//{
+//    /**
+//     * 生成验证码
+//     */
+//    public AjaxResult createCaptcha() throws IOException, CaptchaException;
+//
+//    /**
+//     * 校验验证码
+//     */
+//    public void checkCaptcha(String key, String value) throws CaptchaException;
+//}
